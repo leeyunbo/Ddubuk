ccd202b974fc38e42e8463790fffb3872f220600
leeyunbo <44944031+leeyunbo@users.noreply.github.com>
2019-07-19 00:15:35 +0900

    add progressbar

---
+++ LinePathLastBackUp/Assets/ARLocation/Scripts/UI/Progressbar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Progressbar : MonoBehaviour
+{
+    public Slider progressbar;
+    public float maxvalue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        progressbar.maxValue = maxvalue;
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        progressbar.value += Time.deltaTime * 5;
+        
+    }
+}
