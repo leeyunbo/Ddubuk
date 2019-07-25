aa9879b3868581d78c15c8a2959ae475b1e42eb1
leeyunbo <44944031+leeyunbo@users.noreply.github.com>
2019-07-25 15:14:50 +0900

    fixed TouchEvent for Message Object

--- LinePathLastBackUp/Assets/ARLocation/Scripts/Utils/TouchEvent.cs
+++ LinePathLastBackUp/Assets/ARLocation/Scripts/Utils/TouchEvent.cs
@@ -1,18 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TouchEvent : MonoBehaviour
 {
+    public Text text;
+    public Text UI;
+    public Text RaycastID;
+    public Text cube0ID;
+    public Text cube1ID;
+    public Text cube2ID;
+    public Text cube3ID;
+
+    private Ray ray;
+    private RaycastHit hit;
+
+    public GameObject cube0;
+    public GameObject cube1;
+    public GameObject cube2;
+    public GameObject cube3;
+
     // Start is called before the first frame update
+
     void Start()
     {
-        
+        cube0ID.GetComponent<Text>().text = cube0.GetInstanceID().ToString();
+        cube1ID.GetComponent<Text>().text = cube1.GetInstanceID().ToString();
+        cube2ID.GetComponent<Text>().text = cube2.GetInstanceID().ToString();
+        cube3ID.GetComponent<Text>().text = cube3.GetInstanceID().ToString();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                RaycastID.GetComponent<Text>().text = Mathf.Abs(hit.transform.gameObject.GetInstanceID()).ToString();
+            }
+
+             
+        }
+
         if(Input.touchCount > 0)
         {
             Vector2 pos = Input.GetTouch(0).position;
@@ -24,18 +59,22 @@ public class TouchEvent : MonoBehaviour
             {
                 if(Input.GetTouch(0).phase == TouchPhase.Began)
                 {
-                    print("터치 누름");
+                    text.GetComponent<Text>().text = "터치누름";
                 }
                 else if(Input.GetTouch(0).phase == TouchPhase.Moved)
                 {
-                    print("터치 누르고 이동");
+                    text.GetComponent<Text>().text = "터치누르고이동";
                 }
                 else if(Input.GetTouch(0).phase == TouchPhase.Ended)
                 {
-                    print("터치 뗌");
+                    text.GetComponent<Text>().text = "터치뗌";
                 }
-                GameObject obj = hit.collider.gameObject;
-                print(obj.GetInstanceID());
+                if (hit.collider.gameObject != null)
+                {
+                    GameObject obj = hit.collider.gameObject;
+                    print(obj.GetInstanceID());
+                    UI.GetComponent<Text>().text = obj.GetInstanceID().ToString();
+                }
             }
         }
         
